4ce5f8a116669a5db87638b67c1d8f3776ffd37c
Shubham Tiwari <tshubham102@gmail.com>
2022-10-07 18:59:57 +0530

    Bubble Sort implementation

---
+++ ArrayDataStructure/BubbleSort.py
@@ -0,0 +1,16 @@
+# Time Complexity : O(n^2)
+# Method implementation
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        # n-i-1 we are optimizing the loop as after every pass last element would be bigger
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                # swap of the elements
+                arr[j], arr[j +1] = arr[j+1], arr[j]
+    return arr
+
+# Driver code
+arr = [70, 20, 50, 60, 35, 47]
+result = bubbleSort(arr)
+print("Array after using bubble sort is :", result)
